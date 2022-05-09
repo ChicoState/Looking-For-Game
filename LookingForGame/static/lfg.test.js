981605451978d4a12edfb4fef18f2e79022d17a3
Reed <rlagomarsino1@mail.csuchico.edu>
2022-05-09 04:45:56 -0700

    baby tests complete still working on the big bois.. they are harder than I thought.

---
+++ LookingForGame/static/lfg.test.js
@@ -0,0 +1,7 @@
+import { confirmDeleteModal, closeDeleteModal } from './lfg';
+
+describe('closeDeleteModal', () => {
+	it('Smoke Test for closeDeleteModal', () => {
+		expect(true).to.be.true;
+	})
+})
