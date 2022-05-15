a7ad591b6132f1a097becc59aa49f7ae52f35ee9
Reed <rlagomarsino1@mail.csuchico.edu>
2022-05-15 10:39:37 -0700

    model tests

--- LookingForGame/core/tests/models_tests.py
+++ LookingForGame/core/tests/models_tests.py
@@ -1,6 +1,48 @@
 from django.test import TestCase
+from user.models import UserProfile
+from django.contrib.auth.models import User
 
 class TestModels(TestCase):
     def setUp(self):
+        self.username = 'testUser'
+        self.password = '12345'
+        user1 = User.objects.create(username=self.username)
+        user1.set_password(self.password)
+        user1.save()
+        self.username = 'testUser2'
+        self.password = '54321'
+        user2 = User.objects.create(username=self.username)
+        user2.set_password(self.password)
+        user2.save()
         self.group1 = Group.objects.create(
+                group_number = 1,
+                game_master = "Reed",
+                group_name = "Test_Group_Name",
+                campaign = "TestQuest",
+                group_size = "6",
+                age_minimum = "18+",
+                experience_level = "beginner",
+                meeting_frequencies = "Weekly",
+                group_decrption = "This is a test group",
+                schedule = "Meet on Fridays!"
                 )
+        self.pgroup2 = PendingGroup.objects.create(
+                pending_group_id ="2",
+                group_leader = user1,
+                users = user2,
+                group = self.group1
+                )
+        self.message = MessageModel.objects.create(
+                sender = "Bob",
+                message_text = "this is a test message",
+                time = "12:35",
+                group = self.group1
+                )
+        def test_group_built_correctly(self):
+            self.assertEquals(self.group1.group_name, 'Test_Group_name')
+
+        def test_pending_group_built_correctly(self):
+            self.assertEquals(self.pgroup2.group_leader, 'TestUser')
+
+        def test_message_built_correctly(self):
+            self.assertEquals(self.message.message_text, 'this is a test message')
